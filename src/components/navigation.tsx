a269f08744f2bca53428f9470f9bd108026f79b9
John Bentley <jwbentle@gmail.com>
2024-12-26 13:15:49 -0800

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,19 +6,24 @@ import Link from "next/link";
 export const Navigation = () => {
   return (
     <nav className='atf-container sm:pt-10 md:pt-11 m-0 bg-nav drop-shadow flex flex-col md:flex-row items-center justify-between xs:justify-end'>
-      <section className='w-full md:w-1/2' id='logo'>
-        <Link href={"/"}>
-          <Image src='/logo.svg' width='100' height='95' alt='logo' />
-        </Link>
-      </section>
-      <section
-        className='w-full pt-4 pb-8 md:py-0 md:w-1/2 flex flex-row justify-start'
-        id='links-and-phone'
-      >
-        <div className='' id='links'>
-          <Links orientation='horizontal' size='small' />
+      <div className="container mx-auto px-4">
+          <div className="flex h-16 items-center justify-between">
+            <Link href="/" className="text-2xl font-bold text-[#C0392B]">
+              Tacos del Valle
+            </Link>
+            <div className="hidden space-x-8 md:flex">
+              <a href="#menu" className="text-[#C0392B] hover:text-[#C0392B]/70">
+                Menu
+              </a>
+              <a href="#about" className="text-[#C0392B] hover:text-[#C0392B]/70">
+                About
+              </a>
+              <a href="#contact" className="text-[#C0392B] hover:text-[#C0392B]/70">
+                Contact Us
+              </a>
+            </div>
+          </div>
         </div>
-      </section>
     </nav>
   );
 };
